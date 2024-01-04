331d173e404755b8efcb5b794b1cc2587a219fd4
SANJAYSONKARIYA <119073413+SANJAYSONKARIYA@users.noreply.github.com>
2024-01-04 16:54:51 +0530

    Update settings.py

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -81,25 +81,25 @@ WSGI_APPLICATION = 'onlinexam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME': 'Dehradun',  # Replace with your MongoDB database name
-#         'ENFORCE_SCHEMA': False,
-#         'CLIENT': {
-#             'host': 'mongodb+srv://nstidehradun:dehradun@cluster0.ok0bgkw.mongodb.net/',
-#             'username': 'nstidehradun',
-#             'password': 'dehradun',
-#         }
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR ,'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'Dehradun',  # Replace with your MongoDB database name
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb+srv://nstidehradun:dehradun@cluster0.ok0bgkw.mongodb.net/',
+            'username': 'nstidehradun',
+            'password': 'dehradun',
+        }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR ,'db.sqlite3'),
+#     }
+# }
 
 
 
